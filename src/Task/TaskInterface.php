8f83272eafbfdb3bad5c9d9ed12f9cd9a4048785
Vincent van Dijk <info@vincentvandijk.com>
2017-10-25 18:21:29 +0200

    Changed TaskInterface return type.

--- src/Task/TaskInterface.php
+++ src/Task/TaskInterface.php
@@ -8,12 +8,9 @@ interface TaskInterface
     /**
      * Execute task.
      *
-     * Return exit status should be in the range 0 to 254. Status 255 is preserved for PHP and should not be used.
-     * Status 0 should be used when task is executed succesfully.
-     *
      * @param array $data
-     * @return int
+     * @return void
      * @throws TaskException
      */
-    public function execute(array $data): int;
+    public function execute(array $data): void;
 }
